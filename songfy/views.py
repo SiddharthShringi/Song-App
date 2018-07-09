926546a96bcbff2d255ae86151d6291893125593
BlackSamsung <siddharthshringi@gmail.com>
2018-07-09 12:17:48 +0530

    Write following view functions: 1.home 2.artist_lst 3.genre_lst 4.playlist_lst 5.artist_song 6.genre_song 7.playlist_song

--- songfy/views.py
+++ songfy/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Song, Artist, Genre, Playlist
 
-# Create your views here.
+
+def home(request):
+    songs = Song.objects.all().order_by('title')
+    return render(request, 'songfy/home.html', {'songs': songs})
+
+
+def artist_lst(request):
+    artists = Artist.objects.all().order_by('name')
+    return render(request, 'songfy/artist.html', {'artists': artists})
+
+
+def genre_lst(request):
+    genres = Genre.objects.all().order_by('name')
+    return render(request, 'songfy/genre.html', {'genres': genres})
+
+
+def playlist_lst(request):
+    playlists = Playlist.objects.all().order_by('name')
+    return render(request, 'songfy/playlist.html', {'playlists': playlists})
+
+
+def artist_song(request, pk):
+    artist = get_object_or_404(Artist, pk=pk)
+    artists = Artist.objects.all().order_by('name')
+    songs = artist.songs.all()
+    return render(request, 'songfy/artist_song.html', {'songs': songs, 'artists': artists})
+
+
+def genre_song(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    genres = Genre.objects.all().order_by('name')
+    songs = genre.songs.all()
+    return render(request, 'songfy/genre_song.html', {'songs': songs, 'genres': genres})
+
+
+def playlist_song(request, pk):
+    playlist = get_object_or_404(Playlist, pk=pk)
+    playlists = Playlist.objects.all().order_by('name')
+    songs = playlist.song.all()
+    return render(request, 'songfy/playlist_song.html', {'songs': songs, 'playlists': playlists})
