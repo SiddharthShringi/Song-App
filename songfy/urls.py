01e2a1f9b709c26276d016a0a9ec1ddac0965690
BlackSamsung <siddharthshringi@gmail.com>
2018-07-10 17:45:41 +0530

    Add url to edit songs.

--- songfy/urls.py
+++ songfy/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('genres/genre/<int:pk>', views.genre_song, name='genre_song'),
     path('playlists/playlist/<int:pk>', views.playlist_song, name='playlist_song'),
     path('song/new', views.add_song, name='add_song'),
+    path('song/edit', views.edit_song, name='edit_song'),
 ]
