e2c187dd02bf7e10565e04bff93bd84015e6ce3a
BlackSamsung <siddharthshringi@gmail.com>
2018-08-03 10:44:53 +0530

    make serailzer file

---
+++ songfy/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from songfy.models import Song, Artist, Playlist, Genre
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ('title', 'duration', 'genre', 'artist', 'user')
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ('name', 'user')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'user')
+
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = ('name', 'song', 'user')
