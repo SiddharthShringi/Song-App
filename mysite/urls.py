3c29c2f8bcf0bd8984a8dd2b36761c848d981957
BlackSamsung <siddharthshringi@gmail.com>
2018-08-03 14:57:40 +0530

    add login/logout functionality in browsable api

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('songfy.urls', namespace='songfy')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
